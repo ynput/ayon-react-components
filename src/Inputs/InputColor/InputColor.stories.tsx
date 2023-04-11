2ec32254a109047b4c611471b7d7537d974b1212
Innders <49156310+Innders@users.noreply.github.com>
2023-04-11 16:53:19 +0100

    better InputColor stories

--- src/Inputs/InputColor/InputColor.stories.tsx
+++ src/Inputs/InputColor/InputColor.stories.tsx
@@ -9,10 +9,52 @@ export default meta
 
 type Story = StoryObj<typeof InputColor>
 
-export const Default: Story = {
+// HEX
+export const HEX: Story = {
   args: {
-    value: '#000000',
+    value: '#14d25d',
+    format: 'hex',
+  },
+}
+
+// HEX with ALpha
+export const HEXA: Story = {
+  args: {
+    value: '#14d25dBB',
     alpha: true,
-    onChange: () => console.log('Color Changed'),
+    format: 'hex',
+  },
+}
+
+// unit8
+export const Uint8: Story = {
+  args: {
+    value: [255, 145, 56],
+    format: 'uint8',
+  },
+}
+
+// unit8 with alpha
+export const Uint8A: Story = {
+  args: {
+    value: [255, 145, 56, 0.8],
+    alpha: true,
+    format: 'uint8',
+  },
+}
+
+// Float
+export const Float: Story = {
+  args: {
+    value: [0.5, 0.3, 0.9],
+    format: 'float',
+  },
+}
+
+// unit16
+export const Uint16: Story = {
+  args: {
+    value: [65535, 24580, 14456],
+    format: 'uint16',
   },
 }
