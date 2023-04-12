f3b806df97b54567b9e2febc49ad6d84db848769
Innders <49156310+Innders@users.noreply.github.com>
2023-04-12 12:00:50 +0100

    exports

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,51 @@
 
 // BUTTONS
 export { Button } from './Button'
+
+// dropdown
+export { Dropdown } from './Dropdown'
+
+// FileUpload
+export { FileUpload } from './FileUpload'
+
+// INPUT
+// text
+export { InputText } from './Inputs/InputText'
+// textarea
+export { InputTextarea } from './Inputs/InputTextarea'
+// color
+export { InputColor } from './Inputs/InputColor'
+// number
+export { InputNumber } from './Inputs/InputNumber'
+// switch
+export { InputSwitch } from './Inputs/InputSwitch'
+// password
+export { InputPassword } from './Inputs/InputPassword'
+
+// LAYOUT
+// divider
+export { Divider } from './Layout/Divider'
+// formLayout
+export { FormLayout } from './Layout/FormLayout'
+// formRow
+export { FormRow } from './Layout/FormRow'
+// section
+export { Section } from './Layout/Section'
+// spacer
+export { Spacer } from './Layout/Spacer'
+// toolbar
+export { Toolbar } from './Layout/Toolbar'
+
+// OVERLAY
+// dialog
+export { Dialog } from './Overlay/Dialog'
+// loaderShade
+export { LoaderShade } from './Overlay/LoaderShade'
+
+// PANELS
+// panel
+export { Panel } from './Panels/Panel'
+// scrollPanel
+export { ScrollPanel } from './Panels/ScrollPanel'
+// tablePanel
+export { TablePanel } from './Panels/TablePanel'
