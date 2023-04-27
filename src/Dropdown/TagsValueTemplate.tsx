9a75f49fdc856698ab8444f1a09dd1ac720efbff
Innders <49156310+Innders@users.noreply.github.com>
2023-04-27 10:08:02 +0100

    fix: Dropdown fixes

--- src/Dropdown/TagsValueTemplate.tsx
+++ src/Dropdown/TagsValueTemplate.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import DefaultValueTemplate, { DefaultValueTemplateProps } from './DefaultValueTemplate'
+import { DefaultValueTemplateProps, DefaultValueTemplate } from '.'
 import styled from 'styled-components'
 
 const TagStyled = styled.span`
