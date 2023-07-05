1ede96a3e7131f884a1b2c66217728ac0b067b90
Innders <49156310+Innders@users.noreply.github.com>
2023-07-05 10:24:14 +0100

    feature: user feedback

--- src/FileUpload/FileUpload.stories.tsx
+++ src/FileUpload/FileUpload.stories.tsx
@@ -1,7 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { CustomFile, FileUpload, FileUploadProps } from '.'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { Panel } from '../Panels/Panel'
+import { Button } from '../Button'
 
 const meta: Meta<typeof FileUpload> = {
   component: FileUpload,
@@ -14,10 +15,39 @@ type Story = StoryObj<typeof FileUpload>
 
 const Template = (args: FileUploadProps) => {
   const [files, setFiles] = useState<CustomFile[]>([])
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isFinished, setIsFinished] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  // if true, set to false after 1s
+  if (isSubmitting) {
+    setTimeout(() => {
+      setIsSubmitting(false)
+      setIsFinished(true)
+      // 50% chance of success
+      const isSuccess = Math.random() > 0.5
+      setIsSuccess(isSuccess)
+      if (isSuccess) setFiles([])
+    }, 1000)
+  }
+
+  const handleSubmit = () => {
+    setIsSubmitting(true)
+    setIsSuccess(false)
+    setIsFinished(false)
+  }
 
   return (
     <Panel style={{ maxWidth: 700, height: 300 }}>
-      <FileUpload {...args} files={files} setFiles={setFiles} />
+      <FileUpload
+        {...args}
+        files={files}
+        setFiles={setFiles}
+        isFetching={isSubmitting}
+        isSuccess={isFinished && isSuccess}
+        isError={isFinished && !isSuccess}
+        onSubmit={handleSubmit}
+      />
     </Panel>
   )
 }
@@ -37,6 +67,15 @@ export const SingleSeqExtensions: Story = {
     allowMultiple: false,
     allowSequence: true,
     validExtensions: ['jpg', 'png'],
+  },
+  render: Template,
+}
+
+export const CustomSave: Story = {
+  args: {
+    allowMultiple: true,
+    allowSequence: true,
+    saveButton: <Button icon="magic_button">Custom Save</Button>,
   },
   render: Template,
 }
