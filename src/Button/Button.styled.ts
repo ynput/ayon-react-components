0f33400f4b4262611e4e13e9b8eb254b37184d33
Innders <49156310+Innders@users.noreply.github.com>
2023-08-25 12:51:12 +0100

    fix: button uses props instead of classNames for states

--- src/Button/Button.styled.ts
+++ src/Button/Button.styled.ts
@@ -5,6 +5,8 @@ export type ButtonProps = {
   label?: string
   $link?: boolean
   $icon?: boolean
+  $variant: 'surface' | 'tonal' | 'filled' | 'text' | 'tertiary' | 'nav'
+  $selected: boolean
 }
 
 // STYLES
@@ -54,154 +56,169 @@ export const Button = styled.button<ButtonProps>`
     outline: 2px solid var(--md-sys-color-primary);
   }
 
-  &.surface,
-  &.tonal,
-  &.filled,
-  &.text {
-    /* selected default */
-    &.selected {
+  &:disabled {
+    opacity: 0.5;
+  }
+
+  /* SURFACE: surface-container */
+  ${({ $variant }) =>
+    $variant === 'surface' &&
+    css`
+      background-color: var(--md-sys-color-surface-container-highest);
+      color: var(--md-sys-color-on-surface);
+
+      &:hover {
+        background-color: var(--md-sys-color-surface-container-highest-hover);
+      }
+      &:active {
+        background-color: var(--md-sys-color-surface-container-highest-active);
+      }
+
+      /* disabled default */
+      &:disabled {
+        opacity: 0.5;
+
+        /* remove hover and active styles */
+        &:hover,
+        &:active {
+          background-color: var(--md-sys-color-surface-container-highest);
+        }
+      }
+    `}
+
+  /* TONAL = secondary-container */
+  ${({ $variant }) =>
+    $variant === 'tonal' &&
+    css`
+      background-color: var(--md-sys-color-secondary-container);
+      color: var(--md-sys-color-on-secondary-container);
+
+      &:hover {
+        background-color: var(--md-sys-color-secondary-container-hover);
+      }
+      &:active {
+        background-color: var(--md-sys-color-secondary-container-active);
+      }
+      &:disabled {
+        background-color: var(--md-sys-color-secondary-container);
+      }
+    `}
+
+  /* FILLED = primary */
+  ${({ $variant }) =>
+    $variant === 'filled' &&
+    css`
+      background-color: var(--md-sys-color-primary);
+      color: var(--md-sys-color-on-primary);
+
+      &:hover {
+        background-color: var(--md-sys-color-primary-hover);
+      }
+      &:active {
+        background-color: var(--md-sys-color-primary-active);
+      }
+      &:disabled {
+        background-color: var(--md-sys-color-primary);
+      }
+      &:focus-visible {
+        outline-color: white;
+      }
+    `}
+
+  /* NAV = primary */
+  ${({ $variant, $selected }) =>
+    $variant === 'nav' &&
+    css`
+      border: 1px solid transparent;
+      background-color: transparent;
+      color: var(--md-sys-color-on-surface);
+
+      &:hover {
+        background-color: var(--md-sys-color-background);
+      }
+      &:active {
+        background-color: var(--md-sys-color-background);
+        border-color: var(--md-sys-color-outline-variant);
+      }
+
+      ${$selected &&
+      css`
+        background-color: var(--md-sys-color-background);
+        border-color: var(--md-sys-color-outline-variant);
+      `}
+
+      &:disabled {
+        border: 1px solid transparent;
+        background-color: transparent;
+      }
+    `}
+
+  /* TEXT = surface-container */
+  ${({ $variant }) =>
+    $variant === 'text' &&
+    css`
+      background-color: transparent;
+
+      &:hover {
+        background-color: var(--md-sys-color-surface-container-highest-hover);
+      }
+      &:active {
+        background-color: var(--md-sys-color-surface-container-highest-active);
+      }
+
+      &:disabled {
+        background-color: transparent;
+      }
+    `}
+
+  /* TERTIARY = secondary-container */
+  ${({ $variant, $selected }) =>
+    $variant === 'tertiary' &&
+    css`
+      background-color: var(--md-sys-color-tertiary);
+      color: var(--md-sys-color-on-tertiary);
+
+      &:hover {
+        background-color: var(--md-sys-color-tertiary-hover);
+      }
+      &:active {
+        background-color: var(--md-sys-color-tertiary-active);
+      }
+      &:focus-visible {
+        outline-color: white;
+      }
+
+      ${$selected &&
+      css`
+        background-color: var(--md-sys-color-tertiary-container);
+        color: var(--md-sys-color-on-tertiary-container);
+
+        &:hover {
+          background-color: var(--md-sys-color-tertiary-container-hover);
+        }
+
+        &:active {
+          background-color: var(--md-sys-color-tertiary-container-active);
+        }
+      `}
+
+      &:disabled {
+        background-color: var(--md-sys-color-tertiary);
+      }
+    `}
+
+  /* selection */
+  ${({ $variant, $selected }) =>
+    $selected &&
+    ['surface', 'tonal', 'filled', 'text'].includes($variant) &&
+    css`
       background-color: var(--md-sys-color-primary-container);
       color: var(--md-sys-color-on-primary-container);
 
       &:hover {
         background-color: var(--md-sys-color-primary-container-hover);
       }
-    }
-  }
-
-  &:disabled {
-    opacity: 0.5;
-  }
-
-  /* DEFAULTS */
-
-  /* SURFACE: surface-container */
-  &.surface {
-    background-color: var(--md-sys-color-surface-container-highest);
-    color: var(--md-sys-color-on-surface);
-
-    &:hover {
-      background-color: var(--md-sys-color-surface-container-highest-hover);
-    }
-    &:active {
-      background-color: var(--md-sys-color-surface-container-highest-active);
-    }
-
-    /* disabled default */
-    &:disabled {
-      opacity: 0.5;
-
-      /* remove hover and active styles */
-      &:hover,
-      &:active {
-        background-color: var(--md-sys-color-surface-container-highest);
-      }
-    }
-  }
-  /* TONAL = secondary-container */
-  &.tonal {
-    background-color: var(--md-sys-color-secondary-container);
-    color: var(--md-sys-color-on-secondary-container);
-
-    &:hover {
-      background-color: var(--md-sys-color-secondary-container-hover);
-    }
-    &:active {
-      background-color: var(--md-sys-color-secondary-container-active);
-    }
-    &:disabled {
-      background-color: var(--md-sys-color-secondary-container);
-    }
-  }
-
-  /* FILLED = primary */
-  &.filled {
-    background-color: var(--md-sys-color-primary);
-    color: var(--md-sys-color-on-primary);
-
-    &:hover {
-      background-color: var(--md-sys-color-primary-hover);
-    }
-    &:active {
-      background-color: var(--md-sys-color-primary-active);
-    }
-    &:disabled {
-      background-color: var(--md-sys-color-primary);
-    }
-    &:focus-visible {
-      outline-color: white;
-    }
-  }
-
-  /* FILLED = primary */
-  &.nav {
-    border: 1px solid transparent;
-    background-color: transparent;
-    color: var(--md-sys-color-on-surface);
-
-    &:hover {
-      background-color: var(--md-sys-color-background);
-    }
-    &:active,
-    &.selected {
-      background-color: var(--md-sys-color-background);
-      border-color: var(--md-sys-color-outline-variant);
-    }
-
-    &:disabled {
-      border: 1px solid transparent;
-      background-color: transparent;
-    }
-  }
-
-  /* TEXT = surface-container */
-  &.text {
-    background-color: transparent;
-
-    &:hover {
-      background-color: var(--md-sys-color-surface-container-highest-hover);
-    }
-    &:active {
-      background-color: var(--md-sys-color-surface-container-highest-active);
-    }
-
-    &:disabled {
-      background-color: transparent;
-    }
-  }
-
-  /* TERTIARY = secondary-container */
-  &.tertiary {
-    background-color: var(--md-sys-color-tertiary);
-    color: var(--md-sys-color-on-tertiary);
-
-    &:hover {
-      background-color: var(--md-sys-color-tertiary-hover);
-    }
-    &:active {
-      background-color: var(--md-sys-color-tertiary-active);
-    }
-    &:focus-visible {
-      outline-color: white;
-    }
-
-    &.selected {
-      background-color: var(--md-sys-color-tertiary-container);
-      color: var(--md-sys-color-on-tertiary-container);
-
-      &:hover {
-        background-color: var(--md-sys-color-tertiary-container-hover);
-      }
-
-      &:active {
-        background-color: var(--md-sys-color-tertiary-container-active);
-      }
-    }
-
-    &:disabled {
-      background-color: var(--md-sys-color-tertiary);
-    }
-  }
+    `}
 
   /* icon color should inherit color */
   .icon {
