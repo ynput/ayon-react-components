7bcd077ef4a7e9b66305bf5e380d86f76ec94552
Innders <49156310+Innders@users.noreply.github.com>
2023-08-31 08:09:05 +0100

    fix: improved switch colors

--- src/Inputs/InputSwitch/InputSwitch.styled.ts
+++ src/Inputs/InputSwitch/InputSwitch.styled.ts
@@ -22,14 +22,7 @@ export const Switch = styled.div`
       height: 0;
     }
 
-    input:checked + .slider {
-      background-color: var(--md-sys-color-primary-container);
-    }
-
-    input:checked + .slider:before {
-      transform: translateX(calc(var(--bheight) * 0.8));
-    }
-
+    /* main slider */
     .slider {
       position: absolute;
       cursor: pointer;
@@ -37,10 +30,8 @@ export const Switch = styled.div`
       left: 0;
       right: 0;
       bottom: 0;
-      background-color: var(--md-sys-color-surface-container-low);
-      transition: 0.4s;
+      transition: 0.2s;
       border-radius: calc(var(--bheight) / 2);
-      outline: 1px solid var(--md-sys-color-outline-variant);
 
       &:before {
         position: absolute;
@@ -49,14 +40,44 @@ export const Switch = styled.div`
         width: calc(var(--bheight) * 0.8);
         left: calc(var(--bheight) * 0.1);
         bottom: calc(var(--bheight) * 0.1);
-        background-color: var(--md-sys-color-on-surface);
-        transition: 0.4s;
+        transition: 0.2s;
         border-radius: 50%;
+        scale: 0.8;
+      }
+    }
+
+    /* not check styles */
+    input:not(:checked) + .slider {
+      background-color: var(--md-sys-color-surface-container-highest);
+      outline: 2px solid var(--md-sys-color-outline);
+      :before {
+        background-color: var(--md-sys-color-on-surface);
+      }
+
+      &:hover {
+        background-color: var(--md-sys-color-surface-container-highest-hover);
+      }
+    }
+
+    /* checked styles */
+    input:checked + .slider {
+      background-color: var(--md-sys-color-primary);
+      outline: 2px solid var(--md-sys-color-primary);
+      &::before {
+        transform: translateX(calc(var(--bheight) * 0.65));
+        scale: 1.1;
+        background-color: var(--md-sys-color-on-primary);
+      }
+
+      &:hover {
+        &::before {
+          background-color: var(--md-sys-color-on-primary-hover);
+        }
       }
     }
 
     input:focus-visible + * {
-      outline: 1px solid var(--md-sys-color-primary);
+      outline: 2px solid var(--md-sys-color-tertiary) !important;
     }
   } // switch-body
 `
