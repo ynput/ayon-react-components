28884a8eaf5ac8c480ba9c81676ee2c1c60ce83b
Innders <49156310+Innders@users.noreply.github.com>
2023-10-04 10:53:11 +0100

    enhancement: Switch compact prop for smaller switch

--- src/Inputs/InputSwitch/InputSwitch.tsx
+++ src/Inputs/InputSwitch/InputSwitch.tsx
@@ -5,11 +5,15 @@ import * as Styled from './InputSwitch.styled'
 export interface InputSwitchProps extends InputHTMLAttributes<HTMLInputElement> {
   switchClassName?: string
   switchStyle?: CSSProperties
+  compact?: boolean
 }
 
 export const InputSwitch = forwardRef<HTMLInputElement, InputSwitchProps>(
-  ({ switchStyle, switchClassName, ...props }, ref) => (
-    <Styled.Switch style={switchStyle} className={`${switchClassName} ${props.className}`}>
+  ({ switchStyle, switchClassName, compact, ...props }, ref) => (
+    <Styled.Switch
+      style={switchStyle}
+      className={`${switchClassName} ${props.className} ${compact ? 'compact' : ''}`}
+    >
       <label className="switch-body">
         <input type="checkbox" {...props} ref={ref} />
         <span className="slider"></span>
