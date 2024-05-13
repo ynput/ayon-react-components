6d16c490c2ad4fbd18c06d62ae060273f1f0eb05
Innders <49156310+Innders@users.noreply.github.com>
2024-05-13 09:28:21 +0100

    chore: reverts last commit

--- src/Overlay/Dialog/Dialog.stories.tsx
+++ src/Overlay/Dialog/Dialog.stories.tsx
@@ -34,7 +34,7 @@ const closeProps = {
 const defaultArgs: Story['args'] = {
   header: <HeaderContent />,
   children: <BodyContent />,
-  size: 'full',
+  size: 'md',
   onShow: () => console.log('onShow'),
   closeProps: closeProps,
   hideCancelButton: true,
@@ -87,10 +87,30 @@ export const ColorPicker: Story = {
   },
 }
 
+export const Scroll: Story = {
+  render: Template,
+  args: {
+    children: (
+      <>
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+        <BodyContent />
+      </>
+    ),
+  },
+}
+
 export const NoBackdrop: Story = {
   render: Template,
   args: {
-    isBackdrop: false,
+    hideBackdrop: true,
     hideCancelButton: false,
   },
 }
