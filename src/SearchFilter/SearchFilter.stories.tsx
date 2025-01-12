82edff4fdf157d1e0e93d5546142f63dc8178174
Innders <49156310+Innders@users.noreply.github.com>
2025-01-12 08:27:51 +0000

    feat: search filter component

---
+++ src/SearchFilter/SearchFilter.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SearchFilter } from './SearchFilter'
+import { useState } from 'react'
+import { Filter, Option } from './types'
+
+const meta: Meta<typeof SearchFilter> = {
+  component: SearchFilter,
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof SearchFilter>
+
+const options: Option[] = [
+  {
+    id: 'status',
+    label: 'Status',
+    values: [
+      { id: 'waiting', label: 'Waiting', color: '#FFA500', icon: 'hourglass_empty' },
+      { id: 'inProgress', label: 'In Progress', color: '#4CAF50', icon: 'play_circle' },
+      { id: 'completed', label: 'Completed', color: '#2196F3', icon: 'check_circle' },
+      { id: 'error', label: 'Error', color: '#F44336', icon: 'error' },
+      { id: 'paused', label: 'Paused', color: '#9C27B0', icon: 'pause_circle' },
+      { id: 'cancelled', label: 'Cancelled', color: '#757575', icon: 'cancel' },
+    ],
+  },
+]
+
+const Template = (args: Story['args']) => {
+  const [filters, setFilters] = useState<Filter[]>([])
+
+  return <SearchFilter {...args} options={options} filters={filters} onChange={setFilters} />
+}
+
+export const Default: Story = {
+  args: {},
+  render: Template,
+}
---
+++ src/SearchFilter/types.ts
@@ -0,0 +1,46 @@
+export type FilterValue = {
+  id: string
+  label: string
+  img?: string | null
+  icon?: string | null
+  color?: string | null
+  isCustom?: boolean
+  parentId?: string | null
+}
+
+export type FilterOperator = 'AND' | 'OR'
+
+export type Filter = {
+  id: string
+  type?:
+    | 'string'
+    | 'integer'
+    | 'float'
+    | 'boolean'
+    | 'datetime'
+    | 'list_of_strings'
+    | 'list_of_integers'
+    | 'list_of_any'
+    | 'list_of_submodels'
+    | 'dict'
+  label: string
+  inverted?: boolean
+  operator?: FilterOperator
+  icon?: string | null
+  img?: string | null
+  values?: FilterValue[]
+  isCustom?: boolean
+  isReadonly?: boolean // can not be edited and only removed
+  singleSelect?: boolean
+  fieldType?: string
+}
+
+export interface Option extends Filter {
+  allowNoValue?: boolean // allows the filter to have "no value"
+  allowHasValue?: boolean // allows the filter to have "has a value"
+  allowsCustomValues?: boolean // allows the filter to have custom values
+  allowExcludes?: boolean // allows the filter to be inverted
+  operatorChangeable?: boolean // allows the operator to be changed
+  color?: string | null // color of the filter (not used for root options)
+  parentId?: string | null // parent filter id
+}
