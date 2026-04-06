71d16dac7ead5ad4c4325f47f429f4bb10dd78be
Innders <49156310+Innders@users.noreply.github.com>
2026-04-06 13:06:49 +0100

    fix: filled icon prop

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -1,23 +1,32 @@
 import { forwardRef } from 'react'
 import iconSet from './icons.json'
+import clsx from 'clsx'
+import styled from 'styled-components'
+
+const StyledIcon = styled.span`
+  &.filled {
+    font-variation-settings: 'FILL' 1, 'wght' 200, 'GRAD' 200, 'opsz' 20;
+  }
+`
 
 export type IconType = keyof typeof iconSet
 
 // types
 export interface IconProps extends React.HTMLAttributes<HTMLSpanElement> {
   icon: IconType
+  filled?: boolean
 }
 
 // TODO: link to SVG
 
 export const Icon = forwardRef<HTMLSpanElement, IconProps>((props, ref) => {
   return (
-    <span
+    <StyledIcon
       ref={ref}
       {...props}
-      className={`material-symbols-outlined icon ${props.className || ''}`}
+      className={clsx('material-symbols-outlined icon', props.className, { filled: props.filled })}
     >
       {props.icon}
-    </span>
+    </StyledIcon>
   )
 })
