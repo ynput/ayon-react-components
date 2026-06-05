d947be3c06b6edaf8d96472b6f114bf93a89ea31
Innders <49156310+Innders@users.noreply.github.com>
2026-06-05 07:02:46 +0100

    chore: separate out styles

---
+++ src/SearchFilter/SearchFilterItem/SearchFilterItem.styled.ts
@@ -0,0 +1,77 @@
+import styled from 'styled-components'
+import { Button, theme } from '../..'
+
+export const FilterItem = styled.div`
+  display: flex;
+  align-items: center;
+  gap: var(--base-gap-small);
+  user-select: none;
+  white-space: nowrap;
+
+  background-color: var(--md-sys-color-surface-container-high);
+  padding: 2px 4px;
+  /* padding-right: 8px; */
+  border-radius: 4px;
+
+  cursor: pointer;
+  &:hover {
+    background-color: var(--md-sys-color-surface-container-high-hover);
+
+    .button {
+      background-color: var(--md-sys-color-surface-container-highest-hover);
+    }
+  }
+
+  &.editing {
+    outline: 2px solid #99c8ff;
+  }
+
+  &.disabled {
+    pointer-events: none;
+    opacity: 0.5;
+  }
+`
+
+export const Operator = styled.span`
+  ${theme.labelSmall}
+  display: flex;
+  align-items: center;
+`
+
+export const ChipInput = styled.input`
+  appearance: none;
+  border: none;
+  background: none;
+  font: inherit;
+  color: inherit;
+  padding: 0;
+  margin: 0;
+  min-width: 40px;
+  width: var(--chip-input-width, 60px);
+
+  &:focus {
+    outline: none;
+  }
+`
+
+export const ChipButton = styled(Button)`
+  border-radius: 50%;
+  background-color: unset;
+
+  &:hover:not(.disabled) {
+    &.button {
+      background-color: var(--md-sys-color-primary);
+    }
+    .icon {
+      color: var(--md-sys-color-on-primary);
+    }
+  }
+
+  &.hasIcon {
+    padding: 2px;
+  }
+
+  .icon {
+    font-size: 16px;
+  }
+`
